331e2658a520ab5cee087d550f47449704ac809b
John Bergan <54291092+Funhalver@users.noreply.github.com>
2020-09-11 11:50:36 -0500

    Updated Battlecry and Arrow Storm

--- LOC_Promotions.sql
+++ LOC_Promotions.sql
@@ -9,33 +9,33 @@ INSERT OR REPLACE INTO LocalizedText
 -- Recon
 
 -- Melee
-('LOC_PROMOTION_BATTLECRY_DESCRIPTION', '+7 [ICON_Strength] Combat Strength vs. Melee, Anti-Cavalry and Ranged units.{LOC_BR_LABEL}', 'en_US'),
-('LOC_PROMOTION_TORTOISE_DESCRIPTION', '+10 [ICON_Strength]Combat Strength when defending vs. ranged and air attacks (but not siege units or bombers).{LOC_BR_LABEL}', 'en_US'),
-('LOC_PROMOTION_ZWEIHANDER_DESCRIPTION', '+7 [ICON_Strength]Combat Strength vs. Anti-Cavalry units.{LOC_BR_LABEL}', 'en_US'),
-('LOC_PROMOTION_URBAN_WARFARE_DESCRIPTION', '+10 [ICON_Strength]Combat Strength when fighting in a district or attacking a city or encampment.{LOC_BR_LABEL}', 'en_US'),
-('LOC_PROMOTION_AMPHIBIOUS_DESCRIPTION', 'No [ICON_Strength]Combat Strength penalty when attacking from sea or river. Unaffected by river, embarking, or disembarking [ICON_Movement] Movement costs.{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_BATTLECRY_DESCRIPTION', '+7 [ICON_Strength] Combat Strength when attacking vs. Melee, Anti-Cavalry, and Ranged units.{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_TORTOISE_DESCRIPTION', '+10 [ICON_Strength] Combat Strength when defending vs. ranged and air attacks (but not siege units or bombers).{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_ZWEIHANDER_DESCRIPTION', '+7 [ICON_Strength] Combat Strength vs. Anti-Cavalry units.{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_URBAN_WARFARE_DESCRIPTION', '+10 [ICON_Strength] Combat Strength when fighting in a district or attacking a city or encampment.{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_AMPHIBIOUS_DESCRIPTION', 'No [ICON_Strength] Combat Strength penalty when attacking from sea or river. Unaffected by river, embarking, or disembarking [ICON_Movement] Movement costs.{LOC_BR_LABEL}', 'en_US'),
 
 -- Ranged
 ('LOC_PROMOTION_VOLLEY_DESCRIPTION', '+5 [ICON_Ranged] Ranged Attack Strength vs. land units.{LOC_BR_LABEL}', 'en_US'),
-('LOC_PROMOTION_ARROW_STORM_DESCRIPTION', '+7 [ICON_Ranged] Ranged Attack Strength vs. land and naval units.[NEWLINE]+7 [ICON_Strength]Combat Strength defending vs. land and naval units.{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_ARROW_STORM_DESCRIPTION', '+7 [ICON_Ranged] Ranged Attack Strength vs. land and naval units.{LOC_BR_LABEL}', 'en_US'),
 ('LOC_PROMOTION_INCENDIARIES_DESCRIPTION', '+7 [ICON_Ranged] Ranged Attack Strength vs. cities and encampments.{LOC_BR_LABEL}', 'en_US'),
 
 -- Anti-Cavalry
-('LOC_PROMOTION_SCHILTRON_DESCRIPTION', '+10 [ICON_Strength]Combat Strength when defending vs. Melee units.{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_SCHILTRON_DESCRIPTION', '+10 [ICON_Strength] Combat Strength when defending vs. Melee units.{LOC_BR_LABEL}', 'en_US'),
 
 -- Light Cavalry
-('LOC_PROMOTION_CAPARISON_DESCRIPTION', '+5 [ICON_Strength]Combat Strength vs. Anti-Cavalry units.{LOC_BR_LABEL}', 'en_US'),
-('LOC_PROMOTION_COURSERS_DESCRIPTION', '+5 [ICON_Strength]Combat Strength when attacking Ranged and Siege units.{LOC_BR_LABEL}', 'en_US'),
-('LOC_PROMOTION_SPIKING_THE_GUNS_DESCRIPTION', '+7 [ICON_Strength]Combat Strength when attacking Siege units.{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_CAPARISON_DESCRIPTION', '+5 [ICON_Strength] Combat Strength vs. Anti-Cavalry units.{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_COURSERS_DESCRIPTION', '+5 [ICON_Strength] Combat Strength when attacking Ranged and Siege units.{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_SPIKING_THE_GUNS_DESCRIPTION', '+7 [ICON_Strength] Combat Strength when attacking Siege units.{LOC_BR_LABEL}', 'en_US'),
 ('LOC_PROMOTION_ESCORT_MOBILITY_DESCRIPTION', 'The other units in an Escort Formation units all receive this unit''s [ICON_Movement] Movement speed.{LOC_BR_LABEL}', 'en_US'),
 
 -- Heavy Cavalry
-('LOC_PROMOTION_CHARGE_DESCRIPTION', '+10 [ICON_Strength]Combat Strength vs. a fortified defender.{LOC_BR_LABEL}', 'en_US'),
-('LOC_PROMOTION_BARDING_DESCRIPTION', '+7 [ICON_Strength]Combat Strength when defending vs. ranged and air attacks (but not siege units or bombers).{LOC_BR_LABEL}', 'en_US'),
-('LOC_PROMOTION_MARAUDING_DESCRIPTION', '+7 [ICON_Strength]Combat Strength when fighting in a district or attacking a city or encampment.{LOC_BR_LABEL}', 'en_US'),
-('LOC_PROMOTION_ROUT_DESCRIPTION', '+5 [ICON_Strength]Combat Strength when attacking damaged units.{LOC_BR_LABEL}', 'en_US'),
-('LOC_PROMOTION_ARMOR_PIERCING_DESCRIPTION', '+7 [ICON_Strength]Combat Strength vs. Heavy Cavalry units.{LOC_BR_LABEL}', 'en_US'),
-('LOC_PROMOTION_REACTIVE_ARMOR_DESCRIPTION', '+7 [ICON_Strength]Combat Strength when defending vs Heavy Cavalry and Anti-Cavalry units.{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_CHARGE_DESCRIPTION', '+10 [ICON_Strength] Combat Strength vs. a fortified defender.{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_BARDING_DESCRIPTION', '+7 [ICON_Strength] Combat Strength when defending vs. ranged and air attacks (but not siege units or bombers).{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_MARAUDING_DESCRIPTION', '+7 [ICON_Strength] Combat Strength when fighting in a district or attacking a city or encampment.{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_ROUT_DESCRIPTION', '+5 [ICON_Strength] Combat Strength when attacking damaged units.{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_ARMOR_PIERCING_DESCRIPTION', '+7 [ICON_Strength] Combat Strength vs. Heavy Cavalry units.{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_REACTIVE_ARMOR_DESCRIPTION', '+7 [ICON_Strength] Combat Strength when defending vs Heavy Cavalry and Anti-Cavalry units.{LOC_BR_LABEL}', 'en_US'),
 
 
 -- Siege
@@ -48,9 +48,9 @@ INSERT OR REPLACE INTO LocalizedText
 
 
 -- Naval Melee
-('LOC_PROMOTION_EMBOLON_DESCRIPTION', '+7 [ICON_Strength]Combat Strength vs. naval units.{LOC_BR_LABEL}', 'en_US'),
-('LOC_PROMOTION_REINFORCED_HULL_DESCRIPTION', '+10 [ICON_Strength]Combat Strength when defending vs. ranged and air attacks (but not siege units or bombers).{LOC_BR_LABEL}', 'en_US'),
-('LOC_PROMOTION_CONVOY_DESCRIPTION', '+10 [ICON_Strength]Combat Strength when in an Escort Formation.{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_EMBOLON_DESCRIPTION', '+7 [ICON_Strength] Combat Strength vs. naval units.{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_REINFORCED_HULL_DESCRIPTION', '+10 [ICON_Strength] Combat Strength when defending vs. ranged and air attacks (but not siege units or bombers).{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_CONVOY_DESCRIPTION', '+10 [ICON_Strength] Combat Strength when in an Escort Formation.{LOC_BR_LABEL}', 'en_US'),
 
 -- Naval Ranged
 ('LOC_PROMOTION_ROLLING_BARRAGE_DESCRIPTION', '+10 [ICON_Ranged] Ranged Attack Strength vs. cities and encampments.{LOC_BR_LABEL}', 'en_US'),
@@ -65,14 +65,14 @@ INSERT OR REPLACE INTO LocalizedText
 
 
 -- Air Fighters
-('LOC_PROMOTION_DOGFIGHTING_DESCRIPTION', '+7 [ICON_Strength]Combat Strength vs. Air Fighter units.{LOC_BR_LABEL}', 'en_US'),
-('LOC_PROMOTION_INTERCEPTOR_DESCRIPTION', '+7 [ICON_Strength]Combat Strength vs. Air Bomber units.{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_DOGFIGHTING_DESCRIPTION', '+7 [ICON_Strength] Combat Strength vs. Air Fighter units.{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_INTERCEPTOR_DESCRIPTION', '+7 [ICON_Strength] Combat Strength vs. Air Bomber units.{LOC_BR_LABEL}', 'en_US'),
 ('LOC_PROMOTION_COCKPIT_ARMOR_DESCRIPTION', '+7 [ICON_Ranged] Air Attack Strength vs. Anti-Aircraft units.[NEWLINE]+7 [ICON_Strength]Combat Strength vs Anti-Aircraft damage when attacking tiles they cover.{LOC_BR_LABEL}', 'en_US'),
 ('LOC_PROMOTION_STRAFE_DESCRIPTION', '+17 [ICON_Ranged] Air Attack Strength vs. cities, encampments, and all land and naval units that aren''t Cavalry.[NEWLINE]+17 [ICON_Strength]Combat Strength vs Anti-Aircraft damage when attacking tiles they cover.{LOC_BR_LABEL}', 'en_US'),
 ('LOC_PROMOTION_GROUND_CREWS_DESCRIPTION', 'Heal while deployed.{LOC_BR_LABEL}', 'en_US'),
 
 -- Air Bombers
-('LOC_PROMOTION_BOX_FORMATION_DESCRIPTION', '+7 [ICON_Strength]Combat Strength vs. Air Fighter units.{LOC_BR_LABEL}', 'en_US'),
+('LOC_PROMOTION_BOX_FORMATION_DESCRIPTION', '+7 [ICON_Strength] Combat Strength vs. Air Fighter units.{LOC_BR_LABEL}', 'en_US'),
 ('LOC_PROMOTION_TANK_BUSTER_DESCRIPTION', '+17 [ICON_Ranged] Air Attack Strength vs. Cavalry units.{LOC_BR_LABEL}', 'en_US'),
 ('LOC_PROMOTION_TORPEDO_BOMBER_DESCRIPTION', '+17 [ICON_Ranged] Air Attack Strength vs. naval units.{LOC_BR_LABEL}', 'en_US'),
 ('LOC_PROMOTION_CLOSE_AIR_SUPPORT_DESCRIPTION', '+12 [ICON_Ranged] Air Attack Strength vs. land units.{LOC_BR_LABEL}', 'en_US'),
